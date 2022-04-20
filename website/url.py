ea27fd22728eeb1462132a441a3115119f4d36c6
Kou55555 <20203128@kcska.onmicrosoft.com>
2022-04-20 04:36:19 +0000

    Initial commit

--- website/url.py
+++ website/url.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView,AboutView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('about/', AboutView.as_view()),
 ]
